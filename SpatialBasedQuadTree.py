b0647e2987cb0e057f0232fd3d2e7aebb9b1b6e6
Chandler Coates <ccoates14@georgefox.edu>
2018-09-19 16:37:15 -0700

    updated quad class for base functions

--- SpatialBasedQuadTree.py
+++ SpatialBasedQuadTree.py
@@ -1,4 +1,4 @@
-class spatialQaudTree2D:
+class SpatialQaudTree2D:
 
     itemsAndAssociatedQuads = {}
 
@@ -10,16 +10,34 @@ class spatialQaudTree2D:
         self.storedItems = storedItems
 
         self.quadrantCapacity = quadrantCapacity
+        self.quadrant0 = None
         self.quadrant1 = None
         self.quadrant2 = None
         self.quadrant3 = None
-        self.quadrant4 = None
 
     def add(self, item):
         pass
 
     def chooseQuadToAddTo(self, originX, originY):
-        pass
+
+        if originX < self.originX or originX > self.originX + self.width:
+            raise ValueError("originX not within quad")
+        if originY < self.originY or originY > self.originY + self.length:
+            raise ValueError("OriginY not within quad")
+
+        chooseFromLeftQuads = originX > self.originX + self.width / 2
+        chooseFromTopQuads = originY < self.originY + self.length / 2
+
+        if chooseFromLeftQuads and chooseFromTopQuads:
+            if self.quadrant0 == None:
+                self.quadrant0 = SpatialQaudTree2D()
+        elif not chooseFromLeftQuads and chooseFromTopQuads:
+            quadToChoose = 1
+        elif chooseFromLeftQuads and not chooseFromTopQuads:
+            quadToChoose = 2
+        elif not chooseFromLeftQuads and not chooseFromTopQuads:
+            quadToChoose = 3
+
 
     def isWithinCapacity(self):
         return len(self.storedItems) + 1 < self.quadrantCapacity
